ae901a9a0109121341e85766ae947f2c8fdb0056
osmanbozoglu <iosmanbozoglu@gmail.com>
2022-04-17 21:35:05 +0300

    Consumer configuration was added.

--- src/main/java/com/spring/kafka/demo/SpringKafkaDemoApplication.java
+++ src/main/java/com/spring/kafka/demo/SpringKafkaDemoApplication.java
@@ -1,7 +1,10 @@
 package com.spring.kafka.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringKafkaDemoApplication {
@@ -10,4 +13,12 @@ public class SpringKafkaDemoApplication {
 		SpringApplication.run(SpringKafkaDemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			for (int i = 0; i < 100; i++) {
+				kafkaTemplate.send("KafkaTopic", "hello kafka :)" + i);
+			}
+		};
+	}
 }
